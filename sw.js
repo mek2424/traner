6ab537c7756e10f2e7fbb81e990baa848b51471e
mek2424 <tom.horacek20@gmail.com>
2026-07-18 16:22:23 +0200

    Add files via upload

--- sw.js
+++ sw.js
@@ -1,5 +1,5 @@
-const CACHE_NAME = "trener-v4";
-const APP_SHELL = ["./", "./index.html", "./manifest.json", "./icon-192.png", "./icon-512.png", "./club-logo.png"];
+const CACHE_NAME = "trener-v5";
+const APP_SHELL = ["./", "./index.html", "./manifest.json", "./icon-192.png", "./icon-512.png", "./club-logo.png", "./raptor-sound.mp3"];
 
 self.addEventListener("install", (event) => {
   event.waitUntil(
