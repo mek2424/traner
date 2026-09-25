83c00ec602724c4d5d5f4133cf82664c8bfaec53
mek2424 <tom.horacek20@gmail.com>
2026-09-25 23:56:59 +0200

    Add files via upload

--- sw.js
+++ sw.js
@@ -1,4 +1,4 @@
-const CACHE_NAME = "trener-v36";
+const CACHE_NAME = "trener-v37";
 const APP_SHELL = ["./", "./index.html", "./training-mode.css", "./manifest.json", "./icon-192.png", "./icon-512.png", "./icon-512-maskable.png", "./club-logo.png", "./raptor-sound.mp3", "./Boxing%20Bell%20Sound%20Effect.mp3"];
 
 /* =========================================================
